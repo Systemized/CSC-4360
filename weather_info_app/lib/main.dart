91fff41df3d1f1f91b7914b00effe96d176ba5a9
pjasmat <pjasmat@users.noreply.github.com>
2025-09-15 18:46:08 -0400

    Initial project setup and structure

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -1,91 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: _TabsNonScrollableDemo(),
-      ),
+      title: 'Weather Info App',
+      theme: ThemeData(colorSchemeSeed: Colors.blue, useMaterial3: true),
+      home: const WeatherInfoPage(),
     );
   }
 }
 
-class _TabsNonScrollableDemo extends StatefulWidget {
+class WeatherInfoPage extends StatefulWidget {
+  const WeatherInfoPage({super.key});
+
   @override
-  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
+  State<WeatherInfoPage> createState() => _WeatherInfoPageState();
 }
 
-class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
-    with SingleTickerProviderStateMixin, RestorationMixin {
-  late TabController _tabController;
-
-  final RestorableInt tabIndex = RestorableInt(0);
-
-  @override
-  String get restorationId => 'tab_non_scrollable_demo';
-
-  @override
-  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(tabIndex, 'tab_index');
-    _tabController.index = tabIndex.value;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      initialIndex: 0,
-      length: 3,
-      vsync: this,
-    );
-    _tabController.addListener(() {
-      setState(() {
-        tabIndex.value = _tabController.index;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    tabIndex.dispose();
-    super.dispose();
-  }
-
+class _WeatherInfoPageState extends State<WeatherInfoPage> {
   @override
   Widget build(BuildContext context) {
-// For the To do task hint: consider defining the widget and name of the tabs here
-    final tabs = ['Tab 1', 'Tab 2', 'Tab 3'];
-
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(
-          'Tabs Demo',
+      appBar: AppBar(title: const Text('Weather Info')),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            'Enter a city to fetch weather.',
+            textAlign: TextAlign.center,
+          ),
         ),
-        bottom: TabBar(
-          controller: _tabController,
-          isScrollable: false,
-          tabs: [
-            for (final tab in tabs) Tab(text: tab),
-          ],
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-// hint for the to do task:Considering creating the different for different tabs
-          for (final tab in tabs)
-            Center(
-              child: Text(tab),
-            ),
-        ],
       ),
     );
   }
